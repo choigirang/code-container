858fb7751dab5cf35d906e9c54408d22a317e9dc
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-15 18:33:40 +0900

    init 세팅

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Background from "./Components/Common/Background";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Background />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Type/type.ts
@@ -0,0 +1,4 @@
+// 스택 타입
+export interface Stack {
+  [key: string]: string;
+}
