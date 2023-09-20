c8a1dcd43170cb9348fa340c5c3f4d8a9b862ab3
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-20 13:17:23 +0900

    css 기본 스타일링 제거 및 redux 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import "./App.css";
-import Background from "./Components/Common/Background";
+import "./style/App.css";
+import Background from "./components/common/Background";
+import StackBox from "./components/common/StackBox";
+import { styled } from "styled-components";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Provider store={store}>
+      <BasicApp>
+        <StackBox />
         <Background />
-      </header>
-    </div>
+      </BasicApp>
+    </Provider>
   );
 }
+
+const BasicApp = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 calc((100% - 1500px) / 2);
+`;
 
 export default App;
