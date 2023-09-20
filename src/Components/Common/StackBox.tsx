c938964e02ead099bb135938e08748e9a78e14a1
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-20 13:17:56 +0900

    components setting

--- src/Components/Common/StackBox.tsx
+++ src/Components/Common/StackBox.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { keyframes, styled } from "styled-components";
+import StackList from "./StackList";
+import FolderOfStack from "./FolderOfStack";
 
 export default function StackBox() {
-  return <div>StackBox</div>;
+  return (
+    <Container>
+      <StackList />
+      <FolderOfStack />
+    </Container>
+  );
 }
+const animateBorder = keyframes`
+  0% {
+    border-color: #00bd00;
+  }
+  50% {
+    border-color: #216700;
+  }
+  100% {
+    border-color: #00bd00;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 80%;
+  border: solid 4px #00bd00;
+  border-radius: 3px;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  animation: ${animateBorder} 2s linear infinite; /* border 애니메이션 적용 */
+`;
---
+++ src/components/common/StackItem.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { styled } from "styled-components";
+
+export default function StackItem() {
+  return <div>StackItem</div>;
+}
+
+const EachStack = styled;
