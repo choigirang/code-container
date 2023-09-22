cc1b1a535693e3d0554a96b4e4223685acd203db
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-22 12:40:17 +0900

    feat: api data

--- src/components/common/FolderOfStack.tsx
+++ src/components/common/FolderOfStack.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store/store";
+import { api } from "../../util/api";
 
 export default function FolderOfStack() {
+  // const [apiStack, setApiStack] = useState("");
+  // const stack = useSelector((state: RootState) => state.stack);
+
+  // useEffect(() => {
+  //   const dataOfStack = api.get("");
+  //   setApiStack("");
+  // }, [stack]);
+
+  // if (!apiStack) return <div>데이터가 없습니다.</div>;
+
   return <div>FolderOfStack</div>;
 }
