15f3a9b999cac236b348d999ff4192acf971c134
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-22 09:22:42 +0900

    feat: add redux dispatch

--- src/components/common/StackList.tsx
+++ src/components/common/StackList.tsx
@@ -1,34 +1,65 @@
 import React from "react";
 import { styled } from "styled-components";
-import { frontend } from "../../constant/stackList";
-import { useDispatch, useSelector } from "react-redux";
+import { frontend, backend } from "../../constant/stackList";
+import { useDispatch } from "react-redux";
+import { selectStack } from "../../redux/actions/stack";
 
 export default function StackList() {
   const frontStack = frontend;
-
+  const backStack = backend;
   const dispatch = useDispatch();
 
+  // redux 선택한 stack 저장하기
   const handleSelectStack = (stack: string) => {
-    useSelector;
+    dispatch(selectStack(stack));
   };
   return (
     <Container>
-      {/* 프론트엔드 아이템 */}
-      <p className="title">FRONTEND</p>
-      {Object.keys(frontStack).map((stack) => (
-        <li key={stack} onClick={() => handleSelectStack(stack)}>
-          {frontStack[stack]}
-        </li>
-      ))}
-      {/* 백엔드 아이템 */}
-      <p className="title">BACKEND</p>
+      {/* 프론트엔드 목록 */}
+      <Title>FRONTEND</Title>
+      <DividStack>
+        {Object.keys(frontStack).map((stack) => (
+          <StackItem key={stack} onClick={() => handleSelectStack(stack)}>
+            {frontStack[stack]}
+          </StackItem>
+        ))}
+      </DividStack>
+      {/* 백엔드 목록 */}
+      <Title>BACKEND</Title>
+      <DividStack>
+        {Object.keys(backStack).map((stack) => (
+          <StackItem key={stack} onClick={() => handleSelectStack(stack)}>
+            {backStack[stack]}
+          </StackItem>
+        ))}
+      </DividStack>
     </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
   padding: 20px;
   color: white;
   border-right: solid 3px rgba(255, 255, 255, 0.4);
 `;
+
+const DividStack = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.p`
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  font-size: 18px;
+  font-family: 500;
+  letter-spacing: 1.5;
+`;
+
+const StackItem = styled.li``;
