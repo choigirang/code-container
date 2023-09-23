fe77807f2d61f28fed8d811d89973c3254d771ed
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-23 14:41:46 +0900

    임시 배포

--- front/src/components/common/FolderOfStack.tsx
+++ front/src/components/common/FolderOfStack.tsx
@@ -4,15 +4,15 @@ import { RootState } from "../../redux/store/store";
 import { api } from "../../util/api";
 
 export default function FolderOfStack() {
-  const [apiStack, setApiStack] = useState("");
-  const stack = useSelector((state: RootState) => state.stack);
+  // const [apiStack, setApiStack] = useState("");
+  // const stack = useSelector((state: RootState) => state.stack);
 
-  useEffect(() => {
-    const dataOfStack = api.get("");
-    setApiStack("");
-  }, [stack]);
+  // useEffect(() => {
+  //   const dataOfStack = api.get("");
+  //   setApiStack("");
+  // }, [stack]);
 
-  if (!apiStack) return <div>데이터가 없습니다.</div>;
+  // if (!apiStack) return <div>데이터가 없습니다.</div>;
 
   return <div>FolderOfStack</div>;
 }
