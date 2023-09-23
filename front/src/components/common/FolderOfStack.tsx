f3c800a6187f4c76cd570d28c26420dea6feaed6
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-23 20:54:51 +0900

    feat: 임시 api 데이터 받아와서 화면에 보여주기
    선택된 데이터 없을 시 NoneData 컴포넌트

--- front/src/components/common/FolderOfStack.tsx
+++ front/src/components/common/FolderOfStack.tsx
@@ -2,17 +2,43 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store/store";
 import { api } from "../../util/api";
+import { styled } from "styled-components";
+import { ApiStackData } from "../../type/aboutRedux";
+
+type ContainerProps = {
+  $displayOpt: undefined | ApiStackData[];
+};
 
 export default function FolderOfStack() {
-  // const [apiStack, setApiStack] = useState("");
-  // const stack = useSelector((state: RootState) => state.stack);
+  const [apiStack, setApiStack] = useState<ApiStackData[] | undefined>(
+    undefined
+  );
+  const stack = useSelector((state: RootState) => state.stack.stack);
 
-  // useEffect(() => {
-  //   const dataOfStack = api.get("");
-  //   setApiStack("");
-  // }, [stack]);
+  useEffect(() => {
+    const dataOfStack = api.get("");
+    !dataOfStack && setApiStack(undefined);
+  }, [stack]);
 
-  // if (!apiStack) return <div>데이터가 없습니다.</div>;
-
-  return <div>FolderOfStack</div>;
+  return (
+    <Container $displayOpt={apiStack}>
+      {!apiStack && <NoneData>데이터가 없습니다.</NoneData>}
+      {/* {apiStack?.map()} */}
+    </Container>
+  );
 }
+
+const Container = styled.div<ContainerProps>`
+  width: 100%;
+  display: ${(props) => (props.$displayOpt ? "grid" : "flex")};
+`;
+
+// 데이터 없을 시
+const NoneData = styled.div`
+  width: 100%;
+  color: white;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
