27190a7a43704d414de57d03c1cf3d0cf6907c89
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-24 18:50:49 +0900

    feat: 리덕스 액션 타입 지정

--- front/src/type/aboutRedux.ts
+++ front/src/type/aboutRedux.ts
@@ -1,10 +1,13 @@
 import { selectStack } from "../redux/actions/stack";
-import { superAccount } from "../redux/actions/superAccout";
+import { changeAuthority } from "../redux/actions/authority";
+import { changeWrite } from "../redux/actions/write";
 
 // stack 타입
 export type SelectStackAction = ReturnType<typeof selectStack>;
 // super 타입
-export type SuperAccountAction = ReturnType<typeof superAccount>;
+export type SuperAccountAction = ReturnType<typeof changeAuthority>;
+// write 타입
+export type ChangeWriteAction = ReturnType<typeof changeWrite>;
 
 // stack 타입
 export type Stack = {
