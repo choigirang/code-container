d581e91b4380a96a2bf14c8b5ab0415decc75d5f
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-24 18:50:27 +0900

    feat: super, write 리듀서 추가 combineReducers

--- front/src/redux/store/store.ts
+++ front/src/redux/store/store.ts
@@ -1,8 +1,12 @@
 import { createStore, combineReducers } from "redux";
 import { stackReducer } from "../reducers/stackReducer";
+import { accountReducer } from "../reducers/accountReducer";
+import { writeReducer } from "../reducers/writeReducer";
 
 const rootReducer = combineReducers({
   stack: stackReducer,
+  super: accountReducer,
+  write: writeReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
