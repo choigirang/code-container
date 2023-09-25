be0065087d37ddeef7a174d31d7488b59e86c394
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-25 23:08:06 +0900

    feat: editor 코드 변경

--- front/src/components/etc/Editor.tsx
+++ front/src/components/etc/Editor.tsx
@@ -1,87 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { ContentState, convertToRaw, EditorState } from "draft-js";
+import React, { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import draftToHtml from "draftjs-to-html";
-
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import styled from "styled-components";
-import htmlToDraft from "html-to-draftjs";
+import { EditorState } from "draft-js";
 
-interface IEditor {
-  htmlStr: string;
-  setHtmlStr: React.Dispatch<React.SetStateAction<string>>;
-}
+const MyBlock = styled.div`
+  color: #f1f1f1;
 
-export default function PostEditor({ htmlStr, setHtmlStr }: IEditor) {
-  const [editorState, setEditorState] = useState<EditorState>(() =>
-    EditorState.createEmpty()
-  );
+  .wrapper-class {
+    width: 50%;
+    margin: 0 auto;
+    margin-bottom: 4rem;
+  }
+  .editor {
+    height: 500px !important;
+    border: 1px solid #f1f1f1 !important;
+    padding: 5px !important;
+    border-radius: 2px !important;
+  }
+`;
 
-  useEffect(() => {
-    const blockFromHtml = htmlToDraft(htmlStr);
-    if (blockFromHtml) {
-      const { contentBlocks, entityMap } = blockFromHtml;
-      const contentState = ContentState.createFromBlockArray(
-        contentBlocks,
-        entityMap
-      );
-      const editorState = EditorState.createWithContent(contentState);
-      setEditorState(editorState);
-    }
-  }, [htmlStr]);
+export const EditorForm = () => {
+  // useState로 상태관리하기 초기값은 EditorState.createEmpty()
+  // EditorState의 비어있는 ContentState 기본 구성으로 새 개체를 반환 => 이렇게 안하면 상태 값을 나중에 변경할 수 없음.
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-  const onEditorStateChange = (editorState: EditorState) => {
+  const onEditorStateChange = (
+    editorState: React.SetStateAction<EditorState>
+  ) => {
+    // editorState에 값 설정
     setEditorState(editorState);
-    setHtmlStr(draftToHtml(convertToRaw(editorState.getCurrentContent())));
-  };
-
-  const uploadCallback = (file: Blob) => {
-    return new Promise((res, rej) => {
-      const reader = new FileReader();
-
-      reader.onloadend = async () => {
-        const formData = new FormData();
-        formData.append("multipartFiles", file);
-        // const res = await api.post
-        // resolve({data: {link: res.data}})
-      };
-
-      reader.readAsDataURL(file);
-    });
-  };
-
-  const toolbar = {
-    list: { inDropdown: true },
-    textAlign: { inDropdown: true },
-    link: { inDropdown: true },
-    history: { inDropdown: true },
-    image: { uploadCallback: uploadCallback },
-  };
-
-  const localization = {
-    locale: "ko",
   };
 
   return (
-    <>
-      <Container>
-        <Editor
-          editorClassName="editor"
-          toolbarClassName="toolbar"
-          toolbar={toolbar}
-          placeholder="내용을 입력하세요"
-          localization={localization}
-          editorState={editorState}
-          onEditorStateChange={onEditorStateChange}
-        />
-      </Container>
-    </>
+    <MyBlock>
+      <Editor
+        // 에디터와 툴바 모두에 적용되는 클래스
+        wrapperClassName="wrapper-class"
+        // 에디터 주변에 적용된 클래스
+        editorClassName="editor"
+        // 툴바 주위에 적용된 클래스
+        toolbarClassName="toolbar-class"
+        // 툴바 설정
+        toolbar={{
+          // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
+          list: { inDropdown: true },
+          textAlign: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: false },
+        }}
+        placeholder="내용을 작성해주세요."
+        // 한국어 설정
+        localization={{
+          locale: "ko",
+        }}
+        // 초기값 설정
+        editorState={editorState}
+        // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
+        onEditorStateChange={onEditorStateChange}
+      />
+    </MyBlock>
   );
-}
-
-const Container = styled.div`
-  width: 100%;
-  border: solid 1px white;
-  padding: 20px;
-  color: white;
-`;
+};
