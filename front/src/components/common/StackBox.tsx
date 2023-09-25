1f415f14527bc612fddfc08650248dddc48e76a7
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-25 23:07:49 +0900

    feat: editor 변경
    draft js 사용 시 mounted 에러

--- front/src/components/common/StackBox.tsx
+++ front/src/components/common/StackBox.tsx
@@ -4,12 +4,9 @@ import StackList from "./StackList";
 import FolderOfStack from "./FolderOfStack";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store/store";
-import PostEditor from "../etc/Editor";
+import { EditorForm } from "../etc/Editor";
 
 export default function StackBox() {
-  // 작성한 데이터 (markdown)
-  const [htmlStr, setHtmlStr] = useState<string>("");
-
   const write = useSelector((state: RootState) => state.write.write);
 
   return (
@@ -17,11 +14,7 @@ export default function StackBox() {
       {/* 좌측 스택 리스트 */}
       <StackList />
       {/* 우측 스택에 따른 목록, 글 작성 선택 시 작성 페이지로 바뀜 */}
-      {write ? (
-        <PostEditor htmlStr={htmlStr} setHtmlStr={setHtmlStr} />
-      ) : (
-        <FolderOfStack />
-      )}
+      {write ? <EditorForm /> : <FolderOfStack />}
     </Container>
   );
 }
