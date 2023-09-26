2a1393105cf6e9c3bcf4bd2e2273d4f5260baf12
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-09-26 16:50:56 +0900

    feat: add postRouter uploadPost

--- back/src/routes/post.router.ts
+++ back/src/routes/post.router.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import { showPost } from "../controller/post.controller";
+import { showPost, uploadPost } from "../controller/post.controller";
 
 export const postRouter = express.Router();
 
 postRouter.get("/posts/:stack", showPost);
+
+postRouter.post("/posts", uploadPost);
