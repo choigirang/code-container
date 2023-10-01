04cff244dd5346b9bca49e11ad7ee7938e97ef77
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-01 23:17:09 +0900

    feat: 데이터 list 스타일 및 클릭한 stack 데이터 api

--- front/src/components/common/FolderOfStack.tsx
+++ front/src/components/common/FolderOfStack.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store/store";
 import { api } from "../../util/api";
-import { styled } from "styled-components";
+import { keyframes, styled } from "styled-components";
 import { ApiStackData } from "../../type/api";
+import { AiFillFileText } from "react-icons/ai";
 
 type ContainerProps = {
   $displayOpt: undefined | ApiStackData[];
@@ -16,26 +17,54 @@ export default function FolderOfStack() {
   const stack = useSelector((state: RootState) => state.stack.stack);
 
   useEffect(() => {
-    const dataOfStack = api
-      .get(`/posts/${stack ? stack : "all"}`)
-      .then((res) => {
-        if (res.data.length > 0) {
-          setApiStack(res.data);
-        } else setApiStack(undefined);
-      });
+    api.get(`/posts/${stack ? stack : "all"}`).then((res) => {
+      if (res.data.length > 0) {
+        setApiStack(res.data);
+      } else setApiStack(undefined);
+    });
   }, [stack]);
 
   return (
     <Container $displayOpt={apiStack}>
-      {!apiStack && <NoneData>데이터가 없습니다.</NoneData>}
-      {/* {apiStack?.map()} */}
+      {apiStack ? (
+        apiStack.map((data) => (
+          <DataBox key={data.number} className="data-item">
+            <AiFillFileText className="icon" />
+            <p className="title">{data.title}</p>
+            <p className="date">{data.createdAt.slice(0, 10)}</p>
+          </DataBox>
+        ))
+      ) : (
+        <NoneData>데이터가 없습니다.</NoneData>
+      )}
     </Container>
   );
 }
 
+const animateWave = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-5px); /* 파동의 높이 조절 */
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
 const Container = styled.div<ContainerProps>`
   width: 100%;
   display: ${(props) => (props.$displayOpt ? "grid" : "flex")};
+  ${(props) =>
+    props.$displayOpt
+      ? "grid-template-columns: repeat(5, 1fr);" // 그리드 아이템 너비 설정
+      : ""}
+  color: white;
+
+  .data-item {
+    aspect-ratio: 1; // 너비와 높이의 비율을 1:1로 설정
+  }
 `;
 
 // 데이터 없을 시
@@ -46,4 +75,34 @@ const NoneData = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+// 데이터 있을 시
+const DataBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+
+  // 아이콘
+  .icon {
+    width: 75px;
+    height: 75px;
+    color: white;
+    position: relative;
+    animation: ${animateWave} 1s infinite;
+  }
+
+  // 타이틀
+  .title {
+    font-size: 18px;
+    font-weight: 700;
+  }
+
+  // 날짜
+  .date {
+    font-size: 14px;
+  }
 `;
