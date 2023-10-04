355973d0807192ec2bc355d26161f669bc9805f2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-04 13:06:27 +0900

    feat: 리셋 액션 타입 지정, 데이터 초깃값 설정

--- front/src/type/aboutRedux.ts
+++ front/src/type/aboutRedux.ts
@@ -1,9 +1,13 @@
-import { selectStack } from "../redux/actions/stack";
+import { resetData, selectData, selectStack } from "../redux/actions/stack";
 import { changeAuthority } from "../redux/actions/authority";
 import { changeWrite } from "../redux/actions/write";
 
 // stack 타입
 export type SelectStackAction = ReturnType<typeof selectStack>;
+// 선택한 stack의 data 타입
+export type SelectDataAction = ReturnType<typeof selectData>;
+// 선택한 stack의 data 초기화
+export type ResetDataAction = ReturnType<typeof resetData>;
 // super 타입
 export type SuperAccountAction = ReturnType<typeof changeAuthority>;
 // write 타입
@@ -14,11 +18,16 @@ export type Stack = {
   [key: string]: string;
 };
 
+// 선택한 스택 타입
+export type initStackType = {
+  stack: string;
+};
+
 // 전달될 데이터 타입
 export type StackOfData = {
-  title: string;
   number: number;
-  date: string;
-  body: string;
-  category: string;
+  stack: string;
+  title: string;
+  htmlContent: string;
+  createdAt: string;
 };
