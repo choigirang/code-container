c0c96587fbf59d40067376a76f9263d02198d2bf
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-04 13:04:56 +0900

    feat: 클릭한 데이터로 설정해주는 reducer 추가

--- front/src/redux/store/store.ts
+++ front/src/redux/store/store.ts
@@ -2,9 +2,11 @@ import { createStore, combineReducers } from "redux";
 import { stackReducer } from "../reducers/stackReducer";
 import { accountReducer } from "../reducers/accountReducer";
 import { writeReducer } from "../reducers/writeReducer";
+import { stackDataReducer } from "../reducers/stackReducer";
 
 const rootReducer = combineReducers({
   stack: stackReducer,
+  dataOfStack: stackDataReducer,
   super: accountReducer,
   write: writeReducer,
 });
