be9a4a85e91c6e135955fff70a64ecb7594182c2
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-04 13:04:12 +0900

    feat: 선택한 Data reset

--- front/src/redux/actions/stack.ts
+++ front/src/redux/actions/stack.ts
@@ -2,6 +2,7 @@ import { StackOfData } from "../../type/aboutRedux";
 
 const SELECTED_STACK = "stack/SELECT" as const;
 const SELECTED_DATA = "stack/SELECT_DATA" as const;
+const RETURN_DATA = "stack/RETURN_DATA" as const;
 
 export function selectStack(stack: string) {
   return {
@@ -10,9 +11,17 @@ export function selectStack(stack: string) {
   };
 }
 
+// 개별 데이터 선택
 export function selectData(data: StackOfData) {
   return {
     type: SELECTED_DATA,
     payload: data,
+  };
+}
+
+// 선택된 데이터 초기화
+export function resetData() {
+  return {
+    type: RETURN_DATA,
   };
 }
