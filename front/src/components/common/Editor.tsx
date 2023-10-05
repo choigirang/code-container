32be7111c084b0cbf0779fe9d464d934eff91dfc
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-05 21:01:22 +0900

    refactor: 기존 Editor에서 컴포넌트 분리한 에디터로 변경
    Editor (TitleInput, CategorySelect, SubmitBtn)  에서 컴포넌트 분리 후 Editor로 묶음

---
+++ front/src/components/common/Editor.tsx
@@ -0,0 +1,49 @@
+import { ComponentProps, useState } from "react";
+import CategorySelect from "../common/CategorySelect";
+import TitleInput from "../common/TitleInput";
+import SubmitBtn from "./SubmitBtn";
+import { styled } from "styled-components";
+import ToastEditor from "../editor/ToastEditor";
+
+export default function Editor() {
+  // 타이틀
+  const [title, setTitle] = useState<string>("");
+  // 카테고리
+  const [stack, setStack] = useState<string>("");
+  // 작성한 본문
+  const [htmlContent, setHtmlContent] = useState<string>("");
+  // 작성된 내용을 가져오기 위한 ref
+
+  return (
+    <Container>
+      <TitleCategoryBox>
+        {/* 타이틀 입력 */}
+        <TitleInput title={setTitle} />
+        {/* 카테고리 선택 */}
+        <CategorySelect setStack={setStack} />
+      </TitleCategoryBox>
+      {/* 본문 에디터 */}
+      <ToastEditor setHtmlContent={setHtmlContent} />
+      {/* 전송하기 */}
+      <SubmitBtn title={title} stack={stack} htmlContent={htmlContent} />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 20px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+// 타이틀과 카테고리 선택 div
+const TitleCategoryBox = styled.div`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
