1ed05c8619137ff145884593c2abc1eac0db1459
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-06 17:32:32 +0900

    feat: 렌더링 되고 나서 대문자로 작성된 텍스트에 클래스 추가
    대문자로 작성됐다 === 컴포넌트 or 타입 등
    에 대한 클래스를 추가해서 텍스트 색상을 바꾸고자 함
    
    텍스트의 첫 번째 글자 && 작성된 텍스트 앞자리가 큰 따옴표로 시작됨
    
    두 가지 조건을 성립해야됨

--- front/src/components/editor/ToastViewer.tsx
+++ front/src/components/editor/ToastViewer.tsx
@@ -6,12 +6,44 @@ import "@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 
 import Prism from "prismjs";
+import { ComponentProps, useEffect, useRef } from "react";
 
 export default function ToastViewer({ content }: { content: string }) {
+  const viewerRef = useRef<Viewer | null>(null);
+
+  // Viewer가 변경될 때마다 실행되는 useEffect
+  useEffect(() => {
+    // Viewer가 렌더링될 때마다 콜백 함수 실행
+    handleViewerRendered();
+  }, [content]);
+
+  // Viewer가 렌더링된 후 실행되는 콜백 함수
+  const handleViewerRendered = () => {
+    // Viewer 내의 모든 요소 가져오기
+    const viewerContainer = viewerRef.current?.getRootElement();
+    console.log(viewerContainer);
+
+    // 대문자로 작성된 텍스트 요소에 "component" 클래스 추가
+    if (viewerContainer) {
+      const textElements = viewerContainer.querySelectorAll("p, pre");
+      textElements.forEach((element) => {
+        console.log(element);
+        if (
+          element.textContent &&
+          element.textContent === element.textContent.toUpperCase()
+        ) {
+          element.classList.add("component");
+          console.log(element);
+        }
+      });
+    }
+  };
+
   return (
     <Viewer
       initialValue={content || ""}
       theme="dark"
+      ref={viewerRef}
       plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
     />
   );
