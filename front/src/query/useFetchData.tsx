e31a6972aee8daea4093c04210051b919bbbb8c0
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-10 13:57:55 +0900

    feat: query data hook

--- front/src/query/useFetchData.tsx
+++ front/src/query/useFetchData.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { QUERY_KEY } from "../constant/queryKey";
+import { api } from "../util/api";
+import { PostDataType } from "../type/api";
 
-export default function useFetchData() {
-  return <div>FetchData</div>;
+export default function useFetchData(stack: string) {
+  const { data } = useQuery([QUERY_KEY.stack, stack], () =>
+    api.get(`/posts/${stack}`)
+  );
+  return data;
 }
