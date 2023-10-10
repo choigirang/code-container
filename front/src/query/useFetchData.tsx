082f4a9cf62eb4fb611847409b41216a6ce9baea
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-10 19:18:57 +0900

    feat: stack 없을 떄 api

--- front/src/query/useFetchData.tsx
+++ front/src/query/useFetchData.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { useQuery } from "react-query";
 import { QUERY_KEY } from "../constant/queryKey";
 import { api } from "../util/api";
-import { PostDataType } from "../type/api";
 
 export default function useFetchData(stack: string) {
-  const { data } = useQuery([QUERY_KEY.stack, stack], () =>
-    api.get(`/posts/${stack}`)
+  const { data } = useQuery(
+    [QUERY_KEY.stack, stack !== "" ? stack : "all"],
+    () => api.get(`/posts/${stack !== "" ? stack : "all"}`)
   );
-  return data;
+
+  return data && data.data;
 }
