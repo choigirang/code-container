3a3b6552b0fee27590cdf8b0e027512b79e06a04
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-12 13:54:27 +0900

    Update index.tsx

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
+const initialStack = {};
+
+export const AppContext = createContext(initialStack);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
   document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
