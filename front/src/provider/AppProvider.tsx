658d1512b82a8f590bdf14252a4df6bb934cb2c0
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-22 20:59:49 +0900

    create: 여러 context를 묶어줄 AppProvider
    reduce => createElement

---
+++ front/src/provider/AppProvider.tsx
@@ -0,0 +1,26 @@
+// AppProvider.tsx
+import React from "react";
+import { JsxElement } from "typescript";
+
+type ContextProviderProps<T> = {
+  contexts: React.FC<T>[];
+  children: React.ReactNode;
+};
+
+/**
+ * 여러 개의 context를 전달받는 함수
+ * @param {React.Fc} contexts 컨텍스트 컴포넌트
+ * @param {ReactNode} children 하위 컴포넌트
+ */
+export const AppProvider: React.FC<ContextProviderProps<any>> = ({
+  contexts,
+  children,
+}) => {
+  return (
+    <>
+      {contexts.reduce((prev, context) => {
+        return React.createElement(context, { children: prev });
+      }, children)}
+    </>
+  );
+};
