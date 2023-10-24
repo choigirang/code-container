3ec061eb70537695c53665f62a1a101ce653ff2b
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-24 23:35:52 +0900

    feat: 검색 했을 때 / 검색하지 않았을 때, useQuery 나눠 사용하기
    search(Search 컴포넌트에서 이벤트 발생 시) / posts(default)

--- front/src/query/useFetchData.tsx
+++ front/src/query/useFetchData.tsx
@@ -1,11 +1,22 @@
 import { useQuery } from "react-query";
 import { QUERY_KEY } from "../constant/queryKey";
 import { api } from "../util/api";
+import { AxiosResponse } from "axios";
+import { ApiStackData } from "../type/api";
 
+/**
+ * @param stack 선택한 스택
+ * @returns 스택에 따른 데이터 반환 query hooks
+ */
 export default function useFetchData(stack: string) {
-  const { data } = useQuery([stack !== "" ? stack : "all"], () =>
-    api.get(`/posts/${stack !== "" ? stack : "all"}`)
+  // const search = useQuery<AxiosResponse<ApiStackData[]>>(["keyword", stack], () =>
+  //   api.get(`/search?keyword=${stack}`)
+  // );
+
+  const posts = useQuery<AxiosResponse<ApiStackData[]>>(
+    [stack !== "" ? stack : "all"],
+    () => api.get(`/posts/${stack !== "" ? stack : "all"}`)
   );
 
-  return data && data.data;
+  return { posts };
 }
