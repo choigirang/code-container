9b9861363d2cb03053163f536db0725beb228256
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-25 18:00:54 +0900

    feat: delete useState
    훅 안에서 keyword를 변경하여 사용할 시,
    호출하고 있는 keyword가 변경될 떄마다 useQuery도 재실행 시키기 때문에 불필요한 요청발생

--- front/src/query/useSearchData.tsx
+++ front/src/query/useSearchData.tsx
@@ -1,15 +1,13 @@
 import { AxiosResponse } from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ApiStackData } from "../type/api";
 import { useQuery } from "react-query";
 import { api } from "../util/api";
 
-export default function useSearchData() {
-  const [keyword, setKeyword] = useState("");
-
+export default function useSearchData(keyword: string) {
   const search = useQuery<AxiosResponse<ApiStackData[]>>(
     ["keyword", keyword],
     () => api.get(`/search?keyword=${keyword ? keyword : "all"}`)
   );
-  return { keyword, setKeyword, search };
+  return { search };
 }
