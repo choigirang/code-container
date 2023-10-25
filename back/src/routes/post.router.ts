a5060dc5e9c5202b5cb92c612d8ad9e9805d07d9
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-25 13:59:17 +0900

    feat: add search router

--- back/src/routes/post.router.ts
+++ back/src/routes/post.router.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { showPost, uploadPost } from "../controller/post.controller";
+import {
+  searchPost,
+  showPost,
+  uploadPost,
+} from "../controller/post.controller";
 
 export const postRouter = express.Router();
 
 postRouter.get("/posts/:stack", showPost);
+
+postRouter.get("/search", searchPost);
 
 postRouter.post("/posts", uploadPost);
