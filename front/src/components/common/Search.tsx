e16a2feada3f3915faaf2576b69fcea774e22c69
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-25 17:58:25 +0900

    feat: 컴포넌트 합성을 위한 컴포넌트 이동
    좌측인 StackList에서 Search 컴포넌트를 사용하면서 우측에 있는 ShowDataList로 결괏값을 넘겨주려고 하니,
    context or props drilling
    
    컴포넌트 합성을 위해 Search를 ShowDataList의 상위 컴포넌트인 StackBox에서 사용

--- front/src/components/common/Search.tsx
+++ front/src/components/common/Search.tsx
@@ -3,9 +3,14 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { styled } from "styled-components";
 import useSearchData from "../../query/useSearchData";
 
-export default function Search() {
-  const { setKeyword } = useSearchData();
-  const inputRef = useRef<HTMLInputElement | null>(null);
+export default function Search({
+  keyword,
+  setKeyword,
+}: {
+  keyword: string;
+  setKeyword: React.Dispatch<SetStateAction<string>>;
+}) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const keywordHandler: ComponentProps<"input">["onKeyPress"] = (e) => {
     if (e.key === "Enter") {
@@ -14,7 +19,7 @@ export default function Search() {
     }
   };
 
-  const keywordClickHandler: ComponentProps<"svg">["onClick"] = () => {
+  const keywordClickHandler: ComponentProps<"div">["onClick"] = () => {
     const inputValue = inputRef.current ? inputRef.current.value : "";
     setKeyword(inputValue);
   };
@@ -26,37 +31,54 @@ export default function Search() {
         ref={inputRef}
         placeholder="검색어를 입력하세요."
       />
-      <AiOutlineSearch onClick={keywordClickHandler} className="icon" />
+      <Btn onClick={keywordClickHandler}>
+        <AiOutlineSearch className="icon" />
+      </Btn>
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
+  gap: 10px;
   justify-content: space-between;
-  padding: 5px 20px;
-  border: solid 2px green;
-  border-radius: 10px;
-
-  .icon {
-    color: #53d653;
-
-    &:hover {
-      cursor: pointer;
-    }
-  }
+  padding: 20px 20px;
 `;
 
+// 입력칸
 const Input = styled.input`
   width: 100%;
   height: 100%;
   background-color: transparent;
-  border: none;
   outline: none;
   font-size: 15px;
   color: white;
+  border: solid 2px green;
+  border-radius: 10px;
+  padding: 0 20px;
 
   &::placeholder {
     color: gray;
+  }
+`;
+
+// searchBtn
+const Btn = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  height: 100%;
+  cursor: pointer;
+  background-color: #194219;
+  border-radius: 10px;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: #368336;
+
+    .icon {
+      color: white;
+    }
   }
 `;
