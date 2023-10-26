f2a58e8a6b16dc59d784669f546350023e39d519
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-26 23:47:50 +0900

    feat: StackBox의 editData가 있을 시(prePost)

--- front/src/query/useAddPost.tsx
+++ front/src/query/useAddPost.tsx
@@ -4,15 +4,34 @@ import { WriteContext } from "../provider/WriteProvider";
 import { useMutation } from "react-query";
 import { queryClient } from "..";
 
-export default function useAddPost(data: { [key: string]: string }) {
+/**
+ * 데이터를 받아서 post 를 하는 useMuxtation custom hooks
+ * @param data 작성된 데이터 (title, content, stack(category))
+ * @returns mutate
+ */
+export default function useAddPost(data: {
+  title: string;
+  stack: string;
+  htmlContent: string;
+  prePost?: number;
+}) {
   const { setWrite } = useContext(WriteContext);
+
+  // StackBox에서 editPost에 저장된 기존 post의 number
+  const { prePost, stack } = data;
 
   function addPost() {
     return api
       .post("/posts", data)
       .then((res) => {
-        alert("작성이 완료되었습니다.");
-        setWrite((prev) => !prev);
+        // prePost(게시글의 number)있을 시 게시글 수정이라는 의미
+        if (prePost) {
+          alert("수정이 완료되었습니다.");
+          setWrite((prev) => !prev);
+        } else {
+          alert("작성이 완료되었습니다.");
+          setWrite((prev) => !prev);
+        }
       })
       .catch((err) => {
         alert("콘솔 확인");
@@ -22,7 +41,7 @@ export default function useAddPost(data: { [key: string]: string }) {
 
   return useMutation(addPost, {
     onSuccess: () => {
-      queryClient.invalidateQueries(["all"]);
+      queryClient.invalidateQueries([stack ? stack : "all"]);
     },
   });
 }
