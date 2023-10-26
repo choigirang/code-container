c4ca77c1dba87527f01a306bfb65aad787c3e277
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-26 22:27:42 +0900

    feat: StackBox로부터 data전달받아 수정 버튼 클릭 시 수정 데이터 저장

--- front/src/components/common/ShowEachData.tsx
+++ front/src/components/common/ShowEachData.tsx
@@ -1,18 +1,40 @@
-import React, { useContext } from "react";
+import React, { ComponentProps, useContext } from "react";
 import { StackOfData } from "../../type/aboutRedux";
-import { BsArrowLeftCircleFill } from "react-icons/bs";
+import { BsArrowLeftCircleFill, BsPencilFill } from "react-icons/bs";
 import { styled } from "styled-components";
 import ToastViewer from "../editor/ToastViewer";
 import { SelectDataContext } from "../../provider/SelectDataProvider";
+import { AuthContext } from "../../provider/AuthProvider";
+import { ApiStackData } from "../../type/api";
 
 /**
  * StackBox에서 클릭한 데이터가 있는지
  * 클릭한 스택에 대한 data를 context로부터 전달받아
  * @returns 상세 데이터를 포함한 페이지를 보여주는 컴포넌트
  */
-export default function ShowEachData({ data }: { data: StackOfData }) {
+export default function ShowEachData({
+  data,
+  setWrite,
+  setEdit,
+}: {
+  data: StackOfData;
+  setWrite: React.Dispatch<React.SetStateAction<boolean>>;
+  setEdit: React.Dispatch<React.SetStateAction<ApiStackData>>;
+}) {
+  // 뒤로가기 버튼, context data 초기화
   const { initData } = useContext(SelectDataContext);
+
+  // 비밀번호 입력된 user인지 확인
+  const { user } = useContext(AuthContext);
+
+  // ShowDataList에서 클릭받아 전달받은 props
   const { title, stack, htmlContent, createdAt } = data;
+
+  // 글 수정 이벤트
+  const editPost = () => {
+    setWrite(true);
+    setEdit(data);
+  };
 
   return (
     <Container>
@@ -20,6 +42,7 @@ export default function ShowEachData({ data }: { data: StackOfData }) {
         <IconTitle>
           <BsArrowLeftCircleFill className="icon" onClick={initData} />
           <p className="title">{title}</p>
+          {user && <BsPencilFill className="icon" onClick={editPost} />}
         </IconTitle>
         <DateCategory>
           <p className="date">{createdAt.slice(0, 10)}</p>
@@ -51,11 +74,12 @@ const Top = styled.div`
 // icon & title
 const IconTitle = styled.div`
   display: flex;
+  align-items: center;
   gap: 20px;
 
   .icon {
-    width: 30px;
-    height: 30px;
+    width: 24px;
+    height: 24px;
     cursor: pointer;
   }
 
