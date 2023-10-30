99bbc91d68cbc1620b2e064072e490d3b010cc74
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2023-10-30 14:25:23 +0900

    chore: props 추가

--- front/src/components/common/StackBox.tsx
+++ front/src/components/common/StackBox.tsx
@@ -40,7 +40,7 @@ export default function StackBox() {
       <OverFlowStyle>
         <Search keyword={keyword} setKeyword={setKeyword} />
         {write ? (
-          <Editor edit={edit} setEdit={setEdit} />
+          <Editor edit={edit} setEdit={setEdit} data={selectData} />
         ) : // 선택한 데이터가 있을 시 Each, 없을 시 데이터 목록
         selectData.title ? (
           <ShowEachData
