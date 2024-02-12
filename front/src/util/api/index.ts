8ea6d3173d03ed5403d3a6abc04e247cf2c7c50e
GiRang Choi <118104644+choigirang@users.noreply.github.com>
2024-02-12 22:13:18 +0900

    Update index.ts

--- front/src/util/api/index.ts
+++ front/src/util/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://3.35.234.0:8080/",
   // timeout: 5000,
   headers: {
     "Content-Type": "application/json",
